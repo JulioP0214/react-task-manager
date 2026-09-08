400a136516c27dc6ff2ff6aa0ba5218a93395b68
Julio <jpandruby1@yahoo.comm>
2026-09-08 14:59:08 -0500

    adds navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Task Manager</h2>
+
+      <div className="nav-links">
+        <a href="/todos">Todos</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
