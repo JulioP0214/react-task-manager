3e696bf877a6ec360e344f72919b2ce307a11f95
Julio <jpandruby1@yahoo.comm>
2026-09-08 20:30:42 -0500

    configures github pages deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
 
