a3a786df1bdced83a27003fd6981cd1eb9db9e54
Julio <jpandruby1@yahoo.comm>
2026-09-08 19:17:52 -0500

    adds default todo route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TodoPage from "./pages/TodoPage";
 import ContactPage from "./pages/ContactPage";
@@ -11,6 +11,7 @@ function App() {
         <Navbar />
 
         <Routes>
+          <Route path="/" element={<Navigate to="/todos" />} />
           <Route path="/todos" element={<TodoPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
